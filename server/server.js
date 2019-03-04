bb619cfab9eb8cfae54f6eaefa171d8b020f9427
ahmed <sobh6081@gmail.com>
2019-03-04 04:22:23 +0200

    Edit publicPath variable

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const publicPath = path.join(`${__dirname}/public`)
+const publicPath = path.join(`${__dirname}/../public`)
 const port = process.env.PORT || 3000
-
+console.log(publicPath)
 app.use(express.static(publicPath))
 
 app.listen(port, () => {
